f4f58ac8fcd5fe07e0ecd030a03c6d1c185f6583
ChenHX98 <ChenHX1998@126.com>
2022-04-04 00:02:06 +0800

    修改了一些逻辑

--- src/DocGenerater.java
+++ src/DocGenerater.java
@@ -79,19 +79,23 @@ public class DocGenerater {
 
     private void addKindName(Element kindNode, Document document) {
         System.out.println("这是什么类型呢？");
-        System.out.println("1：动画\n2：轻小说\n3：漫画\n4:音乐");
+        System.out.println("0：退出\n1：动画\n2：轻小说\n3：漫画\n4：音乐\n5：电影");
         Scanner scan=new Scanner(System.in);
         String kindname=scan.nextLine();
 
         boolean leag=false;
         while(!leag){
             leag=true;
-           {
+            {
                 switch (kindname){
+                    case "0" -> {
+                        return;
+                    }
                     case "1"->kindname="动画";
                     case "2"->kindname="轻小说";
                     case "3"->kindname="漫画";
                     case "4"->kindname="音乐";
+                    case "5"->kindname="电影";
                     default -> {
                         System.out.println("超出界限，请重新输入");
                         leag=false;
@@ -233,7 +237,7 @@ public class DocGenerater {
     private void addSubFileKindName(Element subFileNode, Document document) {
         Scanner scan=new Scanner(System.in);
         System.out.println("这是什么类型呢？");
-        System.out.println("1：动画\n2：字幕\n3：oped\n4：预告\n5：ost\n6：宣传图\n7：音乐");
+        System.out.println("0:退出\n1：动画\n2：oped\n3：整体预告(pv)\n4：分级预告\n5：特典\n6：ova\n7：menu\n8：动画宣传\n9：字幕\n10：ost\n11：相关图片\n12：音乐\n13：演唱会\n14：Extra Clip from DVD（额外片段）\n15：其他");
         String kindname=scan.nextLine();
         System.out.println("请输入文件名？");
         String dirName=scan.nextLine();
@@ -245,33 +249,68 @@ public class DocGenerater {
             leag=true;
             {
                 switch (kindname){
+                    case "0" -> {
+                        return;
+                    }
                     case "1"-> {
                         kindname = "动画";
                         donghua(subFileNode, document);
                     }
                     case "2"-> {
-                        kindname = "字幕";
-                        zimu(subFileNode, document);
-                    }
-                    case "3"-> {
                         kindname = "oped";
                         oped(subFileNode, document);
                     }
+                    case "3"-> {
+                        kindname = "整体预告";
+                        yugao(subFileNode, document);
+                    }
                     case "4"-> {
-                        kindname = "预告";
+                        kindname = "分级预告";
                         yugao(subFileNode, document);
                     }
                     case "5"-> {
+                        kindname = "特典";
+                        yugao(subFileNode, document);
+                    }
+                    case "6"-> {
+                        kindname = "ova";
+                        yugao(subFileNode, document);
+                    }
+                    case "7"-> {
+                        kindname = "menu";
+                        yugao(subFileNode, document);
+                    }
+                    case "8"-> {
+                        kindname = "动画宣传";
+                        yugao(subFileNode, document);
+                    }
+                    case "9"-> {
+                        kindname = "字幕";
+                        zimu(subFileNode, document);
+                    }
+                    case "10"-> {
                         kindname = "ost";
                         ost(subFileNode, document);
                     }
-                    case "6"-> {
-                        kindname = "宣传图";
+                    case "11"-> {
+                        kindname = "相关图片";
                         xuanchuantu(subFileNode, document);
                     }
-                    case "7"-> {
+                    case "12"-> {
                         kindname = "音乐";
                         yinyue(subFileNode, document);
+                    }
+                    case "13"-> {
+                        kindname = "演唱会";
+                        donghua(subFileNode, document);
+                    }
+                    case "14"-> {
+                        kindname = "额外片段";
+                        donghua(subFileNode, document);
+                    }
+                    case "15"-> {
+                        kindname = "其他";
+                        qita(subFileNode, document);
                     }
                     default -> {
                         System.out.println("超出界限，请重新输入");
@@ -289,9 +328,13 @@ public class DocGenerater {
         subFileNode.appendChild(dirNameNode);
     }
 
+    private void qita(Element subFileNode, Document document) {
+        addSubFileOrignNames(subFileNode,document);
+    }
+
     private void donghua(Element subFileNode, Document document) {
         Scanner scan=new Scanner(System.in);
-        System.out.println("什么语言的动画？");
+        System.out.println("什么语言的动画？def=ja");
         String lang=scan.nextLine();
         if(lang.equals("")){
             lang="ja";
@@ -301,7 +344,7 @@ public class DocGenerater {
         subFileNode.appendChild(langNode);
 
         outer:while (true){
-            System.out.println("有字幕吗？(y=1,n=0,字幕def:zh_cn)");
+            System.out.println("有字幕吗？(y=1,n=0,zh_cn=2,zh_tw=3,ja=4,en=5,def:zh_cn)");
             String subLang=scan.nextLine();
             switch (subLang){
                 case ""-> subLang="zh_cn";
@@ -311,6 +354,18 @@ public class DocGenerater {
                 case "y", "1"->{
                     System.out.println("请输入");
                     subLang=scan.nextLine();
+                }
+                case "2"->{
+                    subLang="zh_cn";
+                }
+                case "3"->{
+                    subLang="zh_tw";
+                }
+                case "4"->{
+                    subLang="ja";
+                }
+                case "5"->{
+                    subLang="en";
                 }
                 default -> {
                     ;
@@ -327,7 +382,7 @@ public class DocGenerater {
     private void zimu(Element subFileNode, Document document) {
         Scanner scan=new Scanner(System.in);
         outer:while (true){
-            System.out.println("什么语言的字幕？(y=1,n=0,字幕def:zh_cn)");
+            System.out.println("什么语言的字幕？(y=1,n=0,zh_cn=2,zh_tw=3,ja=4,en=5,def:zh_cn)");
             String subLang=scan.nextLine();
             switch (subLang){
                 case ""-> subLang="zh_cn";
@@ -337,6 +392,18 @@ public class DocGenerater {
                 case "y", "1"->{
                     System.out.println("请输入");
                     subLang=scan.nextLine();
+                }
+                case "2"->{
+                    subLang="zh_cn";
+                }
+                case "3"->{
+                    subLang="zh_tw";
+                }
+                case "4"->{
+                    subLang="ja";
+                }
+                case "5"->{
+                    subLang="en";
                 }
                 default -> {
                     ;
@@ -380,6 +447,18 @@ public class DocGenerater {
                 case "y", "1"->{
                     System.out.println("请输入");
                     subLang=scan.nextLine();
+                }
+                case "2"->{
+                    subLang="zh_cn";
+                }
+                case "3"->{
+                    subLang="zh_tw";
+                }
+                case "4"->{
+                    subLang="ja";
+                }
+                case "5"->{
+                    subLang="en";
                 }
                 default -> {
                     ;
@@ -457,7 +536,7 @@ public class DocGenerater {
 
     private void addSubFileOrignName(Element subFileNode, Document document) {
         Element originNameNode=document.createElement("origin_name");
-        
+
         addOriginNameFrom(originNameNode,document);
         //addOriginNameName(originNameNode,document);
         addOriginNameNowName(originNameNode,document);
